297bb71ee510d35a565312f2d4e3bf12e79e37b5
Kyle Whang <kylewhang@gmail.com>
2020-11-17 14:43:17 -0500

    usecontext hooks added

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './containers/DashboardContainer';
 import Login from './containers/LoginContainer';
+import { UserContext } from './UserContext';
 
 const App = () => {
   // Enable isLoggedIn for development
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [user, setUser] = useState(null);
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
 
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -14,12 +17,14 @@ const App = () => {
       {isLoggedIn ? (
         <Router>
           <Switch>
-            <Route exact path="/" component={Dashboard} />
-            {/* <Route exact path='/search' component={} /> */}
-            {/* <Route exact path='/camera' component={} /> */}
-            {/* <Route exact path='/message' component={} /> */}
-            {/* <Route exact path='/profile' component={} /> */}
-            <Route path="*" render={() => <h1>404 - Page Not Found</h1>} />
+            <UserContext.Provider value={value}>
+              <Route exact path="/" component={Dashboard} />
+              {/* <Route exact path='/search' component={} /> */}
+              {/* <Route exact path='/camera' component={} /> */}
+              {/* <Route exact path='/message' component={} /> */}
+              {/* <Route exact path='/profile' component={} /> */}
+              <Route path="*" render={() => <h1>404 - Page Not Found</h1>} />
+            </UserContext.Provider>
           </Switch>
         </Router>
       ) : (
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './tailwind.css';
+import { UserContext } from './UserContext';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export function Index() {
+  const { user, setUser } = useContext(UserContext);
+}
