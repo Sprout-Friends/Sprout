19a58adb924aabac4443a69371c334eb0c1bd1a1
chcho2 <chcho87@gmail.com>
2020-11-17 11:58:54 -0500

    working login

--- src/client/containers/LoginContainer.jsx
+++ src/client/containers/LoginContainer.jsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginContainer = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-}
+  const emailOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordOnChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const submit = (path) => {
+    // client-side validation
+    if (email.trim() === '' || password.trim() === '') {
+      return;
+    }
+    console.log(email, password);
+    fetch(`/${path}`, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-type': 'Application/json',
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setEmail('');
+        setPassword('');
+        // if successfully set user in db, do something
+        console.log('successfully set user!');
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div className="signup">
+      <h2>Sign Up</h2>
+      <div className="login-signup-wrapper">
+        <div className="inputs">
+          <div className="input-border">
+            <input
+              className="input-field"
+              id="input-email"
+              type="text"
+              value={email}
+              onChange={emailOnChange}
+              placeholder="Email"
+            ></input>
+          </div>
+          <br></br>
+          <div className="input-border">
+            <input
+              className="input-field"
+              id="input-password"
+              type="password"
+              value={password}
+              onChange={passwordOnChange}
+              placeholder="Password"
+            ></input>
+          </div>
+          <br></br>
+        </div>
+        <div className="buttonDiv">
+          <button
+            type="submit"
+            id="signupButton"
+            className="buttons"
+            onClick={(e) => {
+              submit(e.target.id);
+            }}
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+      <p>Already have an account? Log in</p>
+    </div>
+  );
+};
 
 export default LoginContainer;
