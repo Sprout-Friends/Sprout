942821968547708be8a4f6a764296c2a963c6c21
Sam Kim <samuyyy@gmail.com>
2020-11-17 11:37:53 -0500

    Initialize Dashboard screen

--- src/client/containers/DashboardContainer.jsx
+++ src/client/containers/DashboardContainer.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import UserInfo from '../components/UserInfo';
+import Gallery from '../components/Gallery';
 
-const DashboardContainer = () => {};
+const DashboardContainer = () => {
+  return (
+    <div id="dashboard-container">
+      <UserInfo />
+      <Gallery />
+    </div>
+  );
+};
 
 export default DashboardContainer;
