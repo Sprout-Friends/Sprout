fd3c44d5ff41d084119bdd8a83bb1c73a775357c
Sam Kim <samuyyy@gmail.com>
2020-11-17 22:22:56 -0500

    Have Gallery work for different screens

--- src/client/components/Gallery.jsx
+++ src/client/components/Gallery.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Gallery = () => {
-  return <div id="gallery-container"></div>;
+const Gallery = ({ images, type }) => {
+  const plantsComponents = images.map(({ plantId, url }, i) => (
+    <div className="w-1/3 p-px" key={`gallery-img-${i}`}>
+      <div className="relative pb-full">
+        {type === 'dashboard' ? (
+          <Link to={`/plant/${plantId}`}>
+            <img
+              className="absolute h-full w-full object-cover border rounded-sm"
+              src={url}
+              alt=""
+            />
+          </Link>
+        ) : (
+          <img
+            className="absolute h-full w-full object-cover border rounded-sm"
+            src={url}
+            alt=""
+          />
+        )}
+      </div>
+    </div>
+  ));
+
+  return (
+    <div id="gallery-container" className="flex-grow overflow-scroll">
+      <div
+        id="gallery-images-container"
+        className="flex flex-wrap px-px w-full"
+      >
+        {plantsComponents}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
