9b6b46fc1618bad3369a752a51ea7527dc9abefc
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-11-18 21:02:25 -0500

    WIP

--- src/client/components/AccountPage.jsx
+++ src/client/components/AccountPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 const AccountPage = () => {
+  const handleSignOut = (e) => {
+    e.preventDefault();
+
+    axios.delete('/signout', {}).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="flex-grow overflow-scroll">
-      <div className="flex flex-col items-start w-full px-px text-center">
-        <div className="px-4 py-2 font-bold text-center text-white bg-green-600 border rounded-lg shadow-lg ">
+      <div className="flex flex-col items-center w-full px-px text-center">
+        <div className="px-4 py-2 font-bold text-center text-white bg-green-600 border rounded-lg">
           <label
             htmlFor="profilePicture"
             className="p-8 text-xl text-white align-middle"
@@ -22,6 +31,7 @@ const AccountPage = () => {
 
         <button
           type="button"
+          onClick={handleSignOut}
           className="w-1/3 px-4 py-2 mt-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
         >
           Sign Out
