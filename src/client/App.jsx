77134e4675081727c4b0aab6f55a52de5a66f29c
Kyle Whang <kylewhang@gmail.com>
2020-11-18 20:55:14 -0500

    searchpage added

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,35 +6,34 @@ import Login from './containers/LoginContainer';
 import PlantProfile from './containers/PlantProfileContainer';
 import NewPost from './containers/NewPostContainer';
 import UserProfile from './components/UserProfileHeader';
+import SearchPage from './containers/SearchContainer';
 
 const App = () => {
   // Enable isLoggedIn for development
   // const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
-
-   <SessionProvider>
-    <div id="app-container">
-      <Router>
-        {isLoggedIn ? (
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/plant/:plantId" component={PlantProfile} />
-            {/* <Route exact path='/search' component={} /> */}
-            <Route exact path="/camera" component={NewPost} />
-            {/* <Route exact path='/message' component={} /> */}
-            <Route exact path="/profile" component={UserProfile} />
-            <Route path="*" render={() => <h1>404 - Page Not Found</h1>} />
-          </Switch>
-        ) : (
-          <Login />
-        )}
-      </Router>
-    </div>
-   </SessionProvider>
-
+    <SessionProvider>
+      <div id="app-container">
+        <Router>
+          {isLoggedIn ? (
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/plant/:plantId" component={PlantProfile} />
+              <Route exact path="/search" component={SearchPage} />
+              <Route exact path="/camera" component={NewPost} />
+              {/* <Route exact path='/message' component={} /> */}
+              <Route exact path="/profile" component={UserProfile} />
+              <Route path="*" render={() => <h1>404 - Page Not Found</h1>} />
+            </Switch>
+          ) : (
+            <Login />
+          )}
+        </Router>
+      </div>
+    </SessionProvider>
   );
 };
 
---
+++ src/client/containers/SearchContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import SearchBar from '../components/SearchBar';
+import Gallery from '../components/Gallery';
+
+const SearchContainer = () => {
+  const images = [
+    {
+      id: 1,
+      url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGbZTYvGPrHKfDNpMBsPflT7D5U4r_XKo8NQ&usqp=CAU',
+    },
+    {
+      id: 2,
+      url:
+        'https://toppng.com/uploads/preview/plants-with-sunglasses-cartoon-11550169867suwayop8om.png',
+    },
+  ];
+  return (
+    <div className="flex flex-col h-screen" id="search-container">
+      <SearchBar />
+      <Gallery images={images} type="SearchPage" />
+      <Navigation />
+    </div>
+  );
+};
+
+export default SearchContainer;
